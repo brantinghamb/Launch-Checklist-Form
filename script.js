826dd8dcf004e59ebfb3ca0cc2cb8f2e15e2ab7e
Barrett Brantingham <barrett.brantingham@gmail.com>
2020-01-04 11:20:37 -0600

    Update script file to meet requirements

--- script.js
+++ script.js
@@ -1,13 +1,82 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+       event.preventDefault();
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+       let pilotName = document.querySelector("input[name=pilotName]");
+       let copilotName = document.querySelector("input[name=copilotName]");
+       let fuelLevel = document.querySelector("input[name=fuelLevel]");
+       let cargoMass = document.querySelector("input[name=cargoMass]");
+
+       if (isNaN(cargoMass.value) || isNaN(fuelLevel.value)) {
+           alert("Make sure to select valid inputs!");
+       }
+
+       if (
+       isNaN(pilotName.value) === false ||
+       isNaN(copilotName.value) === false
+       ) {
+           alert("Make sure to select valid inputs!");
+       }
+
+       if (
+       pilotName.value === "" ||
+       copilotName.value === "" ||
+       cargoMass.value === "" ||
+       fuelLevel.value === ""
+       ) {
+           alert("All fields are required!");
+       }
+
+       document.getElementById(
+       "copilotStatus"
+       ).innerHTML = `Co-pilot ${copilotName.value} Ready.`;
+       document.getElementById(
+       "pilotStatus"
+       ).innerHTML = `Pilot ${pilotName.value} Ready.`;
+
+       if (fuelLevel.value < 10000) {
+           document.getElementById("faultyItems").style.visibility = "visible";
+           document.getElementById("launchStatus").innerHTML =
+               "Shuttle not ready for launch";
+           document.getElementById("launchStatus").style.color = "Red";
+           document.getElementById("fuelStatus").innerHTML =
+               "There is not enough fuel for the journey.";
+       }
+
+       if (cargoMass.value > 10000) {
+           document.getElementById("faultyItems").style.visibility = "visible";
+           document.getElementById("launchStatus").innerHTML =
+               "Shuttle not ready for launch";
+           document.getElementById("launchStatus").style.color = "Red";
+           document.getElementById("cargoStatus").innerHTML =
+               "There is too much mass for the shuttle to take off.";
+       }
+
+       if (fuelLevel.value >= 10000 && cargoMass.value <= 10000) {
+           document.getElementById("faultyItems").style.visibility = "visible";
+           document.getElementById("launchStatus").innerHTML =
+               "Shuttle is ready for launch.";
+           document.getElementById("launchStatus").style.color = "Green";
+       }
+   });
+   });
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(
+   function(response) {
+       response.json().then(function(missions) {
+       const mission = missions[0];
+       const div = document.getElementById("missionTarget");
+       div.innerHTML = `
+                       <ol>
+                           <li>Name: ${mission.name}</li>
+                           <li>Diameter: ${mission.diameter}</li>
+                           <li>Star: ${mission.star}</li>
+                           <li>Distance From Earth:${mission.distance}</li>
+                           <li>Number of Moons: ${mission.moons}</li>
+                           <img src="${mission.image}" alt="space image">
+                       </ol>
+                   `;
+       });
+   }
+   );
